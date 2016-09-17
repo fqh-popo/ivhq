99c5bc900e1cb3eb6c2e155db1b70dfa407a4d9e
fqh-popo <fuqiuhong77@gmail.com>
2016-09-18 02:23:22 +1200

    Modify the slide/At a glance style in country-page; Adjust the other pages.

--- _site/assets/vendor/destinations.js
+++ _site/assets/vendor/destinations.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function () {
-
+	
 	// Locate all the content titles
 	var titles = $("#accordion").find("h1");
 	var titleNumber = titles.length;
@@ -26,11 +26,34 @@ $(document).ready(function () {
 		if ( i != titleNumber - 1) { 
 			$("h1#"+titles[i].id).nextUntil("h1").appendTo("#collapse"+i+" .panel-body"); //Move each content to panel-body
 		} else {
-			console.log($("#collapse"+i+" .panel-body").html());
+			//console.log($("#collapse"+i+" .panel-body").html());
 			$("h1#"+titles[i].id).nextUntil("div").appendTo("#collapse"+i+" .panel-body"); //Move the last content to panel-body
 		}
 
 		$("h1#"+titles[i].id).remove(); //Remove all the h1 titles which have been replaced by panel-title
+	} // end of for loop
+
+	//Modify the "At a glance…" panel style
+	if ($("#collapse0") != null) {
+		$("#collapse0 div.panel-body").prepend(
+			'<div class="row">' +
+				'<div class="glance col-md-8">' +
+				'</div>' +
+				'<div class="glance-right col-md-4">' +
+				'</div>' +
+			'</div>'
+		);
+		$("#collapse0 ul").appendTo("#collapse0 .glance");
+		$("#collapse0 p").appendTo("#collapse0 .glance-right");
+
+		//Spread the "At a glance" panel
+		$("#collapse0").attr("class", "panel-collapse collapse in");
+		$("#heading0 a").attr({"aria-expanded": "true", "class": ""});
 	}
+
+
+	//remove the default "previous" and "next", instead of img defined in main.css
+	$("a.slidesjs-previous").html("");
+	$("a.slidesjs-next").html("");
 
 });
